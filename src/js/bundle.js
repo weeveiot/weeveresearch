19c3f663d3c8a280b4bc3a2ac72d36a2ba2dd09b
meyerhoeferb <meyerhoeferb@gmail.com>
2018-07-09 09:55:09 -0400

    Add functionality to navbar.js to pull account and balance of ether.
    Obviously changes in navbar.js are reflected in bundle.js as it needs
    to be prepackaged using browserify to work.
    
    Does not do anything with account or balance yet, simply sets variables
    in the code and also prints them to the web console.

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -36531,6 +36531,9 @@ function extend() {
 },{}],225:[function(require,module,exports){
 var Web3 = require('web3');
 
+var accounts;
+var userAccount;
+
 var web3Provider;
 
 var contract_token;
@@ -36538,6 +36541,8 @@ var contract_factory;
 
 var token_address = '0x1cd91502f6179b6353bf6a89525183a90eca79a1';
 var factory_address = '0xec717ba4d292ecfd2e60f2bdb65a17d86e93faa4';
+
+var balanceETH = 0;
 
 window.addEventListener('load', function() {
 
@@ -36558,14 +36563,37 @@ window.addEventListener('load', function() {
 	console.log("web3: " + web3);
 
 	// get contract data for factory and token
-	$.getJSON('/home/meyerhoeferb/weeve/weeveresearch/build/contracts/weeveToken.json', function(data) {
-		contract_token = new web3.eth.Contract(data, token_address, {from: token_address});
+	$.getJSON('../../build/contracts/weeveToken.json', function(data) {
+		console.log("pulling token contract");
+		contract_token = new web3.eth.Contract(data.abi, token_address);
+		console.log("token: " + contract_token);
 	});
 
-	contract_factory = new web3.eth.Contract('../../build/contracts/weeveFactory.json').at(factory_address);
+	$.getJSON('../../build/contracts/weeveFactory.json', function(data) {
+		console.log("pulling factory contract");
+		contract_factory = new web3.eth.Contract(data.abi, factory_address);
+		console.log("factory: " + contract_factory);
+	});
 
-	console.log("token: " + contract_token);
-	console.log("factory: " + contract_factory);
+	//do rest of things in timeout so that contract objects are loaded
+	setTimeout(function(){
+		console.log("inside timeout");
+
+		accounts = web3.eth.getAccounts();
+		accounts.then(function(result) {
+			userAccount = result[0]
+			console.log(userAccount);
+			web3.eth.getBalance(userAccount, function(err, res) {
+				if(Number(web3.utils.fromWei(res, 'ether')) != balanceETH) {
+					balanceETH = Number(web3.utils.fromWei(res, 'ether'));
+					console.log("balance of current account: " + balanceETH);
+				}
+			});
+		});
+
+	}, 1000);
+
+
 });
 
 },{"web3":213}],226:[function(require,module,exports){
