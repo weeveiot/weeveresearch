9e654351bf717394cf7842a485d60da11ce23dd9
meyerhoeferb <meyerhoeferb@gmail.com>
2018-07-10 09:29:45 -0400

    Add beginning of work to have navbar reflect ethereum account info.
    Works for account and ether balance but not weev balance yet.
    Having trouble for executing contract calls. It might work how I
    have it but metamask might be messing it up because it apparently
    is known to have some problems when using a private chain like I'm using
    for development (ganache). It rejects all transactions I try to execute
    because it thinks the nonces don't match.

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -36543,14 +36543,64 @@ var token_address = '0x1cd91502f6179b6353bf6a89525183a90eca79a1';
 var factory_address = '0xec717ba4d292ecfd2e60f2bdb65a17d86e93faa4';
 
 var balanceETH = 0;
+var weevBalance;
+
+var testTokensRequested = false;
+
+
+
+/************************************ Functions ***************************************************/
+
+//set the account button to the appropriate colors and text
+function setAccountButton() {
+	accounts = web3.eth.getAccounts();
+	accounts.then(function(result) {
+		userAccount = result[0]
+		console.log(userAccount);
+		$("#metamaskButton").text(userAccount);
+	});
+}
+
+function setEthBalance() {
+	web3.eth.getBalance(userAccount, function(err, res) {
+		if(Number(web3.utils.fromWei(res, 'ether')) != balanceETH) {
+			balanceETH = Number(web3.utils.fromWei(res, 'ether'));
+			$('#balanceETH').text(balanceETH);
+			$('#balanceETH').show();
+			console.log("balance of current account: " + balanceETH);
+		}
+	});
+}
+
+
+function getTestTokens() {
+	web3.eth.getTransactionCount(web3.eth.accounts[0], function(errNonce, nonce) {
+		if(!errNonce) {
+			contract_factory.getTestTokens({value: 0, gas: 100000, nonce: nonce},function(errCall, result) {
+				if(!errCall) {
+					testTokensRequested = true;
+					$('#getTokens').hide();
+				}
+				else {
+					testTokensRequested = true;
+					$('#getTokens').hide();
+					console.log(errCall)
+				}
+			});
+		}
+	});
+}
+
+
+
+/************************************** UI *****************************/
 
 window.addEventListener('load', function() {
 
 	// connect to web3
 	if (typeof web3 !== 'undefined') {
-		//web3Provider = web3.currentProvider;
+		web3Provider = web3.currentProvider;
 
-		web3Provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
 		console.log("web3Provider: " + web3Provider);		//FIXME
 	} else {
 		//default to local ganache
@@ -36579,18 +36629,36 @@ window.addEventListener('load', function() {
 	setTimeout(function(){
 		console.log("inside timeout");
 
+		//set the account display
 		accounts = web3.eth.getAccounts();
 		accounts.then(function(result) {
 			userAccount = result[0]
 			console.log(userAccount);
+			$("#metamaskButton").text(userAccount);
+
+			// set the ethereum balance display
 			web3.eth.getBalance(userAccount, function(err, res) {
 				if(Number(web3.utils.fromWei(res, 'ether')) != balanceETH) {
 					balanceETH = Number(web3.utils.fromWei(res, 'ether'));
+					$('#balanceETH').text(balanceETH + " ETH");
+					$('#balanceETH').show();
 					console.log("balance of current account: " + balanceETH);
 				}
 			});
+
+			//set WEEV balance display and button to get test WEEV
+			//weevBalance = contract_token.methods.balanceOf(userAccount).send({from: userAccount});
+			//weevBalance = contract_token.balanceOf(userAccount);
+			//console.log("WEEV of current account: ", weevBalance);
+
+			if (!testTokensRequested) {
+				$('#getTokens').show();
+			}
+
+
 		});
 
+		console.log("end of setTimeout");
 	}, 1000);
 
 
