232d5f77c4e15709d1eac9d81ce550b2f8accd7d
meyerhoeferb <meyerhoeferb@gmail.com>
2018-07-11 08:48:10 -0400

    Add improvements on trying to get the balance of weev.
    We are now getting a number/undefined, but pretty sure both
    of those are wrong.
    
    Also started trying to give test tokens but doesn't work yet. Or
    it might work but we don't know because get balance probably doesn't.
    
    Unsure if it's a problem with how we're calling the contract functions
    or if it's something weird with asynch web3 webapp stuff.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -20,47 +20,6 @@ var testTokensRequested = false;
 
 /************************************ Functions ***************************************************/
 
-//set the account button to the appropriate colors and text
-function setAccountButton() {
-	accounts = web3.eth.getAccounts();
-	accounts.then(function(result) {
-		userAccount = result[0]
-		console.log(userAccount);
-		$("#metamaskButton").text(userAccount);
-	});
-}
-
-function setEthBalance() {
-	web3.eth.getBalance(userAccount, function(err, res) {
-		if(Number(web3.utils.fromWei(res, 'ether')) != balanceETH) {
-			balanceETH = Number(web3.utils.fromWei(res, 'ether'));
-			$('#balanceETH').text(balanceETH);
-			$('#balanceETH').show();
-			console.log("balance of current account: " + balanceETH);
-		}
-	});
-}
-
-
-function getTestTokens() {
-	web3.eth.getTransactionCount(web3.eth.accounts[0], function(errNonce, nonce) {
-		if(!errNonce) {
-			contract_factory.getTestTokens({value: 0, gas: 100000, nonce: nonce},function(errCall, result) {
-				if(!errCall) {
-					testTokensRequested = true;
-					$('#getTokens').hide();
-				}
-				else {
-					testTokensRequested = true;
-					$('#getTokens').hide();
-					console.log(errCall)
-				}
-			});
-		}
-	});
-}
-
-
 
 /************************************** UI *****************************/
 
@@ -85,14 +44,19 @@ window.addEventListener('load', function() {
 	$.getJSON('../../build/contracts/weeveToken.json', function(data) {
 		console.log("pulling token contract");
 		contract_token = new web3.eth.Contract(data.abi, token_address);
+		contract_token.setProvider(web3Provider);
 		console.log("token: " + contract_token);
 	});
 
 	$.getJSON('../../build/contracts/weeveFactory.json', function(data) {
 		console.log("pulling factory contract");
 		contract_factory = new web3.eth.Contract(data.abi, factory_address);
+		contract_factory.setProvider(web3Provider);
 		console.log("factory: " + contract_factory);
 	});
+
+
+
 
 	//do rest of things in timeout so that contract objects are loaded
 	setTimeout(function(){
@@ -116,19 +80,33 @@ window.addEventListener('load', function() {
 			});
 
 			//set WEEV balance display and button to get test WEEV
-			weevBalance = contract_token.methods.balanceOf(userAccount).send({from: userAccount});
-			//weevBalance = contract_token.balanceOf(userAccount);
-			console.log("WEEV of current account: ", weevBalance);
+			contract_token.methods.balanceOf(userAccount).call()
+			.then(function(result){
+				console.log(result);
+			});
 
 			if (!testTokensRequested) {
 				$('#getTokens').show();
 			}
-
 
 		});
 
 		console.log("end of setTimeout");
 	}, 1000);
 
+	let tokenButton = document.querySelector('#getTokens');
+	tokenButton.addEventListener('click', function() {
+		contract_factory.methods.getTestTokens();
+
+		testTokensRequested = true;
+		$('#getTokens').hide();
+
+		//set WEEV balance display and button to get test WEEV
+		contract_token.methods.balanceOf(userAccount).call()
+		.then(function(result){
+			console.log(result.data);
+		});
+
+	});
 
 });
